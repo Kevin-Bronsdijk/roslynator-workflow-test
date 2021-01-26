b126969e8e8df8ccaa475adaae3cadac47f8b20a
Kevin Brondijk <kevin.bronsdijk@agoda.com>
2021-01-26 22:16:37 +0700

    code smells project added

---
+++ src/test/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    public class Class1
+    {
+        public void Main()
+        {
+            // code smells
+            var x = 1; // not used
+            
+            if (x == x) // always true
+                Console.WriteLine("..."); // RCS1007
+        }
+        
+        public void Foo()
+        {
+            ; // RCS1038
+        }
+    }
+}
